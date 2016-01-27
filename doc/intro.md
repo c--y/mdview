76300a140f41dcfaee19428f54925b280cfe6c4d
chenyan <chenyan@baijiahulian.com>
2016-01-27 19:26:49 +0800

    initialize

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mdview
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/mdview/core_test.clj
@@ -0,0 +1,7 @@
+(ns mdview.core-test
+  (:require [clojure.test :refer :all]
+            [mdview.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
